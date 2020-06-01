c7c9a5458ee481972d48c9b1bdb991d860d916e0
Gary Steinmetz <garysteinmetz@yahoo.com>
2020-05-31 21:19:08 -0400

    Final Exam (2019-2020) - Create (incomplete) test layout with a few questions

---
+++ ChhcsFinalExam2020.content.js
@@ -0,0 +1,57 @@
+//difference between 'console.log()' and 'alert()'
+addSingleQuestionToMath(
+  null,
+  "%s is when a program tries to %s.",
+  [
+    ["Duck typing", "figure out what an object is by checking key characteristics"],
+    ["Duck and cover", "overload an external system then disconnect from that system"],
+    ["Duck dinner", "provision the maximum resources that it can"],
+    ["Duck, duck, goose", "identify which component is offline"],
+    ["Duck eggs", "collect integration test results"]
+  ]
+);
+addSingleQuestionToMath(
+  null,
+  "For coding, programmers primarily interact with %s where each character entered from the keyboard corresponds to %s in source code files.",
+  [
+    ["text files", "exactly one character"],
+    ["word processor files", "exactly one character"],
+    ["text files", "an XML element"],
+    ["word processor files", "an XML element"],
+    ["word processor files", "a JSON object"]
+  ]
+);
+addSingleQuestionToMath(
+  null,
+  "Commonly-used text editors for various operating systems include %s for Windows, %s for Mac, and %s for Linux/Unix.",
+  [
+    ["Notepad", "TextEdit", "vi"],
+    ["Notepad", "vi", "TextEdit"],
+    ["Word", "Pages", "LibraOffice"],
+    ["PowerPoint", "Pages", "LibraOffice"],
+    ["Word", "Pages", "Google Docs"]
+  ]
+);
+addSingleQuestionToMath(
+  null,
+  "%s (which is an IDE which stands for %s) is an advanced text editor made especially for software engineers which has powerful features like %s.",
+  [
+    ["IntelliJ", "'Integrated Development Environment'", "debugging and easy integration with Git"],
+    ["Notepad", "'Integrated Development Environment'", "Windows integration"],
+    ["IntelliJ", "'Integrated Development Environment'", "Bitcoin mining"],
+    ["Notepad", "'Isometric Debugging Environment'", "debugging and easy integration with Git"],
+    ["IntelliJ", "'Isometric Debugging Environment'", "debugging and easy integration with Git"]
+  ]
+);
+//Verbal
+addSingleQuestionToVerbal(
+  createPreamble(["Reference - https://www.getrichslowly.org/you-can-negotiate-anything/"], null),
+  "The %s of any negotiation %s.",
+  [
+    ["3 components", "are power, information, and time"],
+    ["2 components", "are charm and determination"],
+    ["only component", "is money"],
+    ["endgame", "is to make more money"],
+    ["3 components", "preparation, demands, and achievement"]
+  ]
+);
